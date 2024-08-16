afd3c4408a0b34c25fb1d23d3997cf1ee5881bfd
ImKai1 <kaigamer.ke@gmail.com>
2024-08-16 18:04:43 -0500

    Added gun scripts

---
+++ Assets/Scripts/Gameplay/DamageGun.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+using UnityEngine;
+
+public class DamageGun : MonoBehaviour
+{
+    [SerializeField] float Damage;
+    [SerializeField] float BulletRange;
+    private Transform PlayerCamera;
+
+    private void Start()
+    {
+        PlayerCamera = Camera.main.transform;
+    }
+
+    public void Shoot()
+    {
+        Ray gunRay = new Ray(PlayerCamera.position, PlayerCamera.forward);
+        if(Physics.Raycast(gunRay, out RaycastHit hitInfo, BulletRange))
+        {
+            if(hitInfo.collider.gameObject.TryGetComponent(out Entity enemy){
+                enemy.Health -= Damage;
+            }
+            
+        }
+    }
+}
