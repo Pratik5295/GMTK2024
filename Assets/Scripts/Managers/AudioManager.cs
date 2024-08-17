83db896cd6c34de79e935f3c64f37f09dd2b1a4d
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2024-08-17 22:52:23 +0530

    Audio Manager prefab and singleton script

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance = null;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
