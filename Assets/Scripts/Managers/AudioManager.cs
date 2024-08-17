c6a466b3640a4d05155fb4bf895a6520f26f123d
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2024-08-17 23:10:54 +0530

    Audio Manager Background Source

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
 
+[DefaultExecutionOrder(1)]
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance = null;
+
+    [Header("Background Audio Sources")]
+    [SerializeField] private AudioSource backgroundSource;
 
     private void Awake()
     {
@@ -16,5 +20,21 @@ public class AudioManager : MonoBehaviour
         }
 
         DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlayBackground(AudioClip clip,float volume = 1f)
+    {
+        backgroundSource.clip = clip;
+        backgroundSource.volume = volume;
+        backgroundSource.loop = true;
+        backgroundSource.Play();
+    }
+
+    public void StopBackground()
+    {
+        if (backgroundSource.isPlaying)
+        {
+            backgroundSource.Stop();
+        }
     }
 }
