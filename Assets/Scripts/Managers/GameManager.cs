71556a2a4b1a01bacd175192a36a9a082c185b5d
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2024-08-17 12:51:32 +0530

    Game Scene Manager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -3,21 +3,24 @@ using UnityEngine;
 
 public enum GameState
 {
-    DEFAULT = 0,
+    DEFAULT = 0,        //Empty state
     GAME = 1,
     PAUSED = 2,
     GAMEOVER = 3
 }
 
+[DefaultExecutionOrder(0)]
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance = null;
 
     [Header("Game State")]
     [SerializeField] private GameState state;
+    [SerializeField] private bool isPaused = false;
 
     public Action<GameState> OnStateChanged;
     public GameState State => state;
+    public bool IsPaused => isPaused;   
 
     private void Awake()
     {
@@ -48,10 +51,12 @@ public class GameManager : MonoBehaviour
     public void PauseGame()
     {
         SetState(GameState.PAUSED);
+        isPaused = true;
     }
 
     public void ResumeGame()
     {
         SetState(GameState.GAME);
+        isPaused = false;
     }
 }
--- Assets/Scripts/Managers/GameSceneManager.cs
+++ Assets/Scripts/Managers/GameSceneManager.cs
@@ -7,11 +7,14 @@ public enum SceneEnum
     GAME = 1
 }
 
+[DefaultExecutionOrder(1)]
 public class GameSceneManager : MonoBehaviour
 {
     public static GameSceneManager Instance = null;
 
     [SerializeField] private SceneEnum activeScene;
+
+    [SerializeField] private GameManager gameManager;
 
     private void Awake()
     {
@@ -28,10 +31,54 @@ public class GameSceneManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
+    private void Start()
+    {
+        gameManager = GameManager.Instance;
+
+        if(gameManager == null)
+        {
+            Debug.LogWarning("Missing Game Manager");
+        }
+        else
+        {
+            gameManager.OnStateChanged += OnGameStateChangedHandler;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (gameManager != null)
+        {
+            gameManager.OnStateChanged -= OnGameStateChangedHandler;
+        }
+    }
+
     public void LoadGameScene(SceneEnum _scene)
     {
         activeScene = _scene;
         SceneManager.LoadScene((int) activeScene);
     }
+
+    private void OnGameStateChangedHandler(GameState _state)
+    {
+        switch(_state)
+        {
+            case GameState.GAME:
+
+                //Start the game scene only if its a new game
+                if(!gameManager.IsPaused)
+                {
+                    LoadGameScene(SceneEnum.GAME);
+                }
+                break;
+
+            case GameState.GAMEOVER:
+
+                LoadGameScene(SceneEnum.MENU);
+                break;
+
+        }
+    }
+
 
 }
