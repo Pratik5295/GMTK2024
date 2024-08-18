3c8a77836f66cc9cc2a37d50702c89274083cd8b
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2024-08-18 16:27:04 +0530

    Player Testing scene equipped with basic health blob

--- Assets/Scripts/UI/UIHealthHandler.cs
+++ Assets/Scripts/UI/UIHealthHandler.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 [DefaultExecutionOrder(4)]
 public class UIHealthHandler : MonoBehaviour
 {
+    [Header("Player Reference")]
+    [SerializeField] private PlayerHealth player;
+
     [Header("Reference to Health Blob")]
     [SerializeField] private GameObject healthBlob;
     [SerializeField] private MeshRenderer blobMesh;
@@ -10,24 +13,35 @@ public class UIHealthHandler : MonoBehaviour
     [Header("Health Blob Visualization Materials")]
     [SerializeField] private Material healthBlobMat;
 
-    [Range(0f, 1f)]
-    [SerializeField] private float health;
-
     private void Start()
     {
         healthBlob = GetComponent<GameObject>();
         blobMesh = GetComponent<MeshRenderer>();
-
         blobMesh.material = healthBlobMat;
+
+        if (player == null)
+        {
+            Debug.LogError("Assign Reference to Player Health script on Player");
+        }
+        else
+        {
+            player.OnPlayerDamageEvent += UpdateCurrentHealth;
+
+            UpdateCurrentHealth(player.GetHealth,player.GetMaxHealth);
+        }
     }
 
-    private void Update()
+    private void OnDestroy()
     {
-        healthBlobMat.SetFloat("_OverlayAmount", health);
+        if (player != null)
+        {
+            player.OnPlayerDamageEvent -= UpdateCurrentHealth;
+        }
     }
 
-    public void SetHealth()
+    public void UpdateCurrentHealth(float _newHealth,float _maxHealth)
     {
-
+        float percentage = _newHealth / _maxHealth;
+        healthBlobMat.SetFloat(MetaConstants.MatShaderOverlay, percentage);
     }
 }
