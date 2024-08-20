f02d528928009ba292287855e07215ef1854f93c
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2024-08-20 21:44:03 +0530

    Pause Menu fix

--- Assets/Scripts/UI/VolumeSliderUI.cs
+++ Assets/Scripts/UI/VolumeSliderUI.cs
@@ -5,15 +5,20 @@ public class VolumeSliderUI : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
 
-    private void Start()
+    private void Awake()
     {
         _slider = GetComponent<Slider>();
     }
 
     private void OnEnable()
     {
+        if (_slider != null)
+        {
+            _slider = GetComponent<Slider>();
+        }
         var value = Mathf.Pow(10, AudioManager.Instance.BG_Volume) / 20;
         _slider.value = value;
+        
     }
 
     public void UpdateSliderValue()
